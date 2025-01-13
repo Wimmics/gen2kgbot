a7fe4d9a9a62b915e0f73485fb788c31b25a1354
YoucTagh <youc.learn.13@gmail.com>
2025-01-13 09:31:51 +0100

    update environment_ubuntu

--- app/core/scenarios/scenario_5/scenario_5.py
+++ app/core/scenarios/scenario_5/scenario_5.py
@@ -36,9 +36,8 @@ from rdflib.plugins.sparql.parser import parseQuery
 logger = setup_logger(__name__)
 
 # openai_api_key = os.getenv("OPENAI_API_KEY")
-# llm = ChatOllama(model="llama3.2:1b")
-# llm = ChatOllama(model="llama3.2:1b")
-llm = ChatOllama(model="mistral:7b")
+llm = ChatOllama(model="llama3.2:1b")
+# llm = ChatOllama(model="mistral:7b")
 # llm = ChatOpenAI(
 #                 model="gpt-4o",
 #                 openai_api_key=openai_api_key,
