3da9d5069d0c61623ebe74b314d9c57f840fac01
Franck Michel <frmichel@users.noreply.github.com>
2025-01-14 11:18:14 +0100

    Change logging config

--- app/core/scenarios/scenario_1/scenario_1.py
+++ app/core/scenarios/scenario_1/scenario_1.py
@@ -1,16 +1,24 @@
+import os
 import argparse
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import MessagesState
 from langchain_core.messages import AIMessage, HumanMessage
 from langgraph.graph import StateGraph, START, END
-from langgraph.graph import MessagesState
-from app.core.utils.printing import new_log
-
+from app.core.utils.utils import setup_logger
 from app.core.scenarios.scenario_1.utils.prompt import PROMPT
 
 
-llm = ChatOllama(model="llama3.2:1b")
+logger = setup_logger(__package__, __file__)
+
+# llm = ChatOllama(model="llama3.2:1b")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI(
+    model="gpt-4o",
+    openai_api_key=openai_api_key,
+)
+logger.info(f"LLM initialized")
 
 # Node
 # def interpret_results(state: MessagesState):
@@ -19,6 +27,7 @@ llm = ChatOllama(model="llama3.2:1b")
 
 
 def interpret_results(state: MessagesState):
+    logger.info(f"Question: {state["messages"]}")
     result = llm.invoke([PROMPT] + state["messages"])
     return {"messages": result}
 
@@ -31,26 +40,26 @@ s1_builder.add_edge("Interpret_results", END)
 
 graph = s1_builder.compile()
 
+
 def main():
 
-    parser = argparse.ArgumentParser(description="Process the scenario with the predifined or custom question.")
-    
-    parser.add_argument('-c', '--custom', type=str,
-                        help="Provide a custom question.")
-    
+    parser = argparse.ArgumentParser(
+        description="Process the scenario with the predifined or custom question."
+    )
+    parser.add_argument("-c", "--custom", type=str, help="Provide a custom question.")
     args = parser.parse_args()
-    
+
     if args.custom:
         question = args.custom
     else:
         question = "What protein targets does donepezil (CHEBI_53289) inhibit with an IC50 less than 10 µM?"
-    
-    state = graph.invoke({"messages":HumanMessage(question)})
 
-    new_log()
+    state = graph.invoke({"messages": HumanMessage(question)})
+
+    logger.info("----------------------------------------------------------------")
     for m in state["messages"]:
-        m.pretty_print()
-    new_log()
+        logger.info(m)
+    logger.info("----------------------------------------------------------------")
 
 
 if __name__ == "__main__":
